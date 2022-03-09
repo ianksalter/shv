419a07e4aff047299a7d014271d9d9cc51fbb2e0
your name <you@example.com>
2022-03-09 13:33:02 +0000

    Submitted for investigation

--- python/create-hole-example.py
+++ python/create-hole-example.py
@@ -27,12 +27,35 @@ bpy.ops.mesh.primitive_cube_add(
     
 bpy.context.object.name = "Wall 1"
 
+wall_1 = bpy.context.scene.objects['Wall 1']
+
 
 # TODO add code for generating holes here:
 
-holes
+hole_1_location = (hole_1_x_start + hole_1_length / 2,
+                   0,
+                   hole_1_z_start + hole_1_height / 2)
+                   
+hole_1_scale = (hole_1_length / 2, 0.1, hole_1_height / 2)                   
 
+# holes
 
+bpy.ops.mesh.primitive_cube_add(
+    enter_editmode=False,
+    align='WORLD',   
+    location=hole_1_location,
+    scale=hole_1_scale)
+    
+bpy.context.object.name = "Hole 1"
+
+hole_1 = bpy.context.scene.objects['Hole 1']
+
+bpy.context.object.display_type = 'WIRE'
+bpy.context.object.hide_render = True
+
+#bpy.ops.outliner.item_activate(deselect_all=True)
+#bpy.context.view_layer.objects.active = bpy.data.objects['Wall 1']
+#bpy.data.objects['Wall 1'].select_set(True)
 
 #Save the generated file
 # bpy.ops.wm.save_as_mainfile(filepath="blender/create-hole-example.blend")
