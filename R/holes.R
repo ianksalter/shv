05d6e1c309d7698345abc18cef4fa7f4757bc2f0
your name <you@example.com>
2022-03-17 11:14:09 +0000

    Implemented hole_locations function.
    SVH-48

--- R/holes.R
+++ R/holes.R
@@ -1,7 +1,7 @@
 #' hole_locations
 #'
 #' Calculates a tibble of absolute hole data given a tibble of walls and a
-#' tibble of relative holedata
+#' tibble of relative hole data
 #'
 #' @param wall_tbl a tibble of wall data
 #' @param relative_hole_tbl tibble a of hole data relative to the walls
@@ -10,13 +10,90 @@
 #' within the walls on blender.
 #' @export
 #' @examples
+#'
+#' name <- c("Wall_1", "Wall_2", "Wall_3", "Wall_4")
+#' start_x <- c(0, 1, 0, 1)
+#' start_y <- c(0, 0, 1, 1)
+#' start_z <- c(0, 0, 0, 0)
+#' wall_length <- c(2, 2, 2, 2)
+#' wall_width <- c(0.1, 0.2, 0.1, 0.1)
+#' wall_height <- c(3, 3, 3, 3)
+#' rotation_z <- c(0, 90, 45, -60)
+#' location_before_rotation_x <- wall_length / 2
+#' location_before_rotation_y <-
+#'   wall_width / 2 * ifelse(rotation_z <= 0, 1, -1) # Pivot correction
+#' location_x <-
+#'   start_x +
+#'   z_rotate_x(location_before_rotation_x,
+#'              location_before_rotation_y,
+#'              rotation_z)
+#' location_y <-
+#'   start_y +
+#'   z_rotate_y(location_before_rotation_x,
+#'              location_before_rotation_y,
+#'              rotation_z)
+#' location_z <- wall_height / 2
 #' wall_tbl <-
-#'   tibble::tribble(
+#'   tibble::tibble(
+#'     name = name,
+#'     location_x = location_x,
+#'     location_y = location_y,
+#'     location_z = location_z,
+#'     length = wall_length,
+#'     width = wall_width,
+#'     height = wall_height,
+#'     start_x = start_x,
+#'     start_y = start_y,
+#'     start_z = start_z,
+#'     rotation_z = rotation_z
+#'   )
+#'
+#' hole_name <- c("Hole_1", "Hole_2", "Hole_3", "Hole_4")
+#' hole_start_x <- c(0.5, 1, 0.5, 1)
+#' hole_start_z <- c(0, 1, 0, 1)
+#' hole_length <- c(1, 0.5, 1, 0.5)
+#' hole_height <- c(2, 1, 2, 1)
+#' wall_name <- c("Wall_1", "Wall_2", "Wall_3", "Wall_4")
+#' relative_hole_tbl <-
+#'   tibble::tibble(
+#'     name = hole_name,
+#'     start_x = hole_start_x,
+#'     start_z = hole_start_z,
+#'     hole_length = hole_length,
+#'     hole_height = hole_height,
+#'     wall_name = wall_name
 #'   )
 #'
 #' hole_locations(wall_tbl, relative_hole_tbl)
 hole_locations <- function(wall_tbl, relative_hole_tbl){
   #TODO write code to create absolute wall tbl
-absolute_hole_tbl <- NULL
+  #join relative hole tibble and wall tibble
+  #calculate the absolute hole tibble from the whole tibble created by joining wall tibble and relative whole tibble
+  # whole_hole_tibble <- dplyr::left_join(relative_hole_tbl, wall_tbl, by = c("wall_name" = "name"))
+
+  whole_hole_tibble <- dplyr::left_join(relative_hole_tbl, wall_tbl, by = c("wall_name" = "name"))
+
+  absolute_hole_tbl <-
+    tibble::tibble(
+      name = whole_hole_tibble$name,
+      location_x =
+        whole_hole_tibble$start_x.y +
+        z_rotate_x(whole_hole_tibble$start_x.x + whole_hole_tibble$hole_length / 2,
+                   whole_hole_tibble$width / 2* ifelse(whole_hole_tibble$rotation_z <= 0, 1, -1),
+                   wall_tbl$rotation_z),
+      location_y =
+        whole_hole_tibble$start_y +
+        z_rotate_y(whole_hole_tibble$start_x.x + whole_hole_tibble$hole_length / 2,
+                   whole_hole_tibble$width / 2* ifelse(whole_hole_tibble$rotation_z <= 0, 1, -1),
+                   wall_tbl$rotation_z),
+      location_z =
+        whole_hole_tibble$start_z.y + whole_hole_tibble$start_z.x + whole_hole_tibble$hole_height / 2,
+      length = whole_hole_tibble$hole_length,
+      width = whole_hole_tibble$width,
+      height = whole_hole_tibble$hole_height,
+      rotation_z = whole_hole_tibble$rotation_z,
+      wall_name = whole_hole_tibble$wall_name
+    )
+
   return(absolute_hole_tbl)
 }
--- tests/testthat/test-holes.R
+++ tests/testthat/test-holes.R
@@ -4,7 +4,8 @@ test_that("Dummy test to get R Studio Run Tests button", {
 
 describe("hole_locations", {
 
-  it("Returns a tibble of existing pillars",
+  it("Calculates a tibble of absolute hole data given a tibble of walls and a
+     tibble of relative hole data",
      {
        name <- c("Wall_1", "Wall_2", "Wall_3", "Wall_4")
        start_x <- c(0, 1, 0, 1)
@@ -14,16 +15,18 @@ describe("hole_locations", {
        wall_width <- c(0.1, 0.2, 0.1, 0.1)
        wall_height <- c(3, 3, 3, 3)
        rotation_z <- c(0, 90, 45, -60)
+       location_before_rotation_x <- wall_length / 2
+       location_before_rotation_y <-
+         wall_width / 2 * ifelse(rotation_z <= 0, 1, -1) # Pivot correction
        location_x <-
          start_x +
-         z_rotate_x(wall_length / 2,
-                    wall_width /2 *
-                      - rotation_z / abs(rotation_z), # Pivot correction
+         z_rotate_x(location_before_rotation_x,
+                    location_before_rotation_y,
                     rotation_z)
        location_y <-
          start_y +
-         z_rotate_y(wall_length / 2,
-                    wall_width / 2 * - rotation_z / abs(rotation_z),
+         z_rotate_y(location_before_rotation_x,
+                    location_before_rotation_y,
                     rotation_z)
        location_z <- wall_height / 2
        wall_tbl <-
@@ -37,7 +40,8 @@ describe("hole_locations", {
            height = wall_height,
            start_x = start_x,
            start_y = start_y,
-           start_z = start_z
+           start_z = start_z,
+           rotation_z = rotation_z
          )
 
        hole_name <- c("Hole_1", "Hole_2", "Hole_3", "Hole_4")
@@ -48,12 +52,12 @@ describe("hole_locations", {
        wall_name <- c("Wall_1", "Wall_2", "Wall_3", "Wall_4")
        relative_hole_tbl <-
          tibble::tibble(
-           name <- hole_name,
-           start_x <- hole_start_x,
-           start_z <- hole_start_z,
-           hole_length <- hole_length,
-           hole_height <- hole_height,
-           wall_name <- wall_name
+           name = hole_name,
+           start_x = hole_start_x,
+           start_z = hole_start_z,
+           hole_length = hole_length,
+           hole_height = hole_height,
+           wall_name = wall_name
          )
 
        hole_location_x <-
@@ -61,7 +65,7 @@ describe("hole_locations", {
           z_rotate_x(
             hole_start_x + hole_length / 2,
             wall_width / 2  * # Not generic need width of the holes wall
-              - rotation_z / abs(rotation_z),
+              ifelse(rotation_z <= 0, 1, -1),
             rotation_z # Not generic need  rotation of the holes wall
           )
        hole_location_y <-
@@ -69,11 +73,11 @@ describe("hole_locations", {
          z_rotate_y(
            hole_start_x + hole_length / 2,
            wall_width / 2 *  # Not generic need width of the holes wall
-             - rotation_z / abs(rotation_z),
+             ifelse(rotation_z <= 0, 1, -1),
            rotation_z # Not generic need  rotation of the holes wall
          )
        hole_location_z <- hole_start_z + hole_height / 2 # Not generic need to add start_z of the holes wall
-       expected_hole_tibble <-
+       expected_hole_tbl <-
          tibble::tibble(
            name = hole_name,
            location_x = hole_location_x,
@@ -86,9 +90,30 @@ describe("hole_locations", {
            wall_name = wall_name # Not generic need the name of the holes wall.
          )
        actual_hole_tbl <- hole_locations(wall_tbl, relative_hole_tbl)
-       # expect_equal(actual_hole_tbl, expected_hole_tbl)
+       expect_equal(actual_hole_tbl, expected_hole_tbl)
 
      }
   )
 
 })
+
+a <- tibble::tribble(
+  ~x1, ~x2,
+  "A", 1,
+  "B", 2,
+  "C", 3
+)
+
+b <- tibble::tribble(
+  ~x1, ~x3, ~x2,
+  "A", "T", 3,
+  "B", "F", 4,
+  "D", "T", 5,
+)
+
+
+
+
+
+
+
